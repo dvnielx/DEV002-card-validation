d26da5fbefa5d4e5abc6fb57dbc125fea898c103
Daniela Castro <dgonzalezcs@outlook.com>
2022-11-10 11:03:58 -0800

    elimine favicon y agregue un header al index2

--- src/validator.js
+++ src/validator.js
@@ -29,12 +29,14 @@ const validator = {
           //suma todos los dígitos de la tarjeta
           agregarDigitos += currentDigit; 
       }
-  }
+  } 
+    // creo otra constante para alojar la suma de los dígitos, ver si el total sacando su módulo es === a 0,
+    //al ser typeOf retorna un tipo de dato primitivo, si no es true, entonces devuelve false
     const total = (agregarDigitos % 10) ===0
     console.log(total, agregarDigitos)
       return total
   },
-
+  // regresa una copia de los números de mi CC. 0 indica el primer elemento, -4 indica desplazamiento desde el final del array, extrae los 4 últimos elementos del array. .replace, reemplaza de forma global por un #
   maskify: (creditCardNumber) => {
   return creditCardNumber.slice(0, -4).replace(/./g, '#') + creditCardNumber.slice(-4)
 }

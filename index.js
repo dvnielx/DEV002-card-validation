529c7837c1713c4f52557e685eabff566fe46512
Daniela Castro <dgonzalezcs@outlook.com>
2023-02-10 12:16:08 -0600

    Updates

--- index.js
+++ index.js
@@ -5,19 +5,18 @@ const numeroMaskify = document.getElementById('numeroMaskify')
 const buttonValidate = document.getElementById('btn-purchase');
 
 buttonValidate.addEventListener("click", function () {
-    console.log("Click se ejecutó");
 
     let numerosCreditCard = document.getElementById("creditCardNumber").value;
 
     if (validator.isValid(numerosCreditCard)) {
         alert("Valid credit card. Thank you for your purchase! Your ticket has been sent to your email :) ");
     } else {
-        alert("Your credit card isn't valid. Try again.");
+        alert("Your credit card " + validator.maskify(ccNumber.value) + " isn't valid. Try again.");
     }
 
 });
-ccNumber.addEventListener("keyup",function() {
+ccNumber.addEventListener("keyup",function() { //keyup: cada vez que sueltan una tecla, este evento sucede.
 
-const traerMaskify = validator.maskify(ccNumber.value)
-numeroMaskify.textContent= traerMaskify
+const traerMaskify = validator.maskify(ccNumber.value) //en esta nueva variable, en mi f maskify, entra el valor de ccNumber
+numeroMaskify.textContent= traerMaskify //el string es devuelto con la propiedad maskify en la interfaz
 });
--- validator.js
+++ validator.js
@@ -1,11 +1,10 @@
 const validator = {
   isValid: (creditCardNumber) => {
   const length = creditCardNumber.length;  //longitud de string
-  console.log("click se ejecutó" + creditCardNumber)
   let agregarDigitos = 0;
   
   //si la cantidad de dígitos, al sacar módulo(%) 2 da como resultado 0
-  if(length % 2 == 0)
+  if(length % 2 == 0) //no necesarimente debe ser para números
   {
       // Comienza al inicio del numero y duplica a partir del primer número
     
@@ -21,22 +20,21 @@ const validator = {
                   // Separar los valores de 2 digitos para después sumarlos
                   let primerNumero = parseInt(currentDigit / 10); //identifica el primer numero de currentDigit tomando el entero, es decir 1 p. ej 16 / 10 = 1.6 = 1
                   let ultimoNumero = currentDigit % 10; //identifica el ultimo número de current digit con modulo 10 p ej, 16 mod 10 = 6
-                  
+                  //confirmar x console log si/en donde se guarda
                   // suma el primero y último número de currentDigit
                   currentDigit = primerNumero + ultimoNumero;
               }
           }
           //suma todos los dígitos de la tarjeta
-          agregarDigitos += currentDigit; 
+          agregarDigitos += currentDigit; //consolidar agregarDigitos | Qué pasa con los impares y menores a 9?
       }
   } 
     // creo otra constante para alojar la suma de los dígitos, ver si el total sacando su módulo es === a 0,
     //al ser typeOf retorna un tipo de dato primitivo, si no es true, entonces devuelve false
     const total = (agregarDigitos % 10) ===0
-    console.log(total, agregarDigitos)
       return total
   },
-  // regresa una copia de los números de mi CC. 0 indica el primer elemento, -4 indica desplazamiento desde el final del array, extrae los 4 últimos elementos del array. .replace, reemplaza de forma global por un #
+  // regresa una copia de los números de mi CC. .slice extrae una sección del string y lo devuelve como un nuevo string sin modificar el original. 0 indica el primer elemento, -4 indica desplazamiento desde el final del array, extrae los 4 últimos elementos del array. .replace, reemplaza de forma global por un #
   maskify: (creditCardNumber) => {
   return creditCardNumber.slice(0, -4).replace(/./g, '#') + creditCardNumber.slice(-4)
 }

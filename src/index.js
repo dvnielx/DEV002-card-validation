4b7a5196908a490d2431bc54058531b7fbbd5622
Daniela Castro <dgonzalezcs@outlook.com>
2023-02-10 12:15:41 -0600

    read.me agregado

--- src/index.js
+++ src/index.js
@@ -5,14 +5,13 @@ const numeroMaskify = document.getElementById('numeroMaskify')
 const buttonValidate = document.getElementById('btn-purchase');
 
 buttonValidate.addEventListener("click", function () {
-    console.log("Click se ejecutó");
 
     let numerosCreditCard = document.getElementById("creditCardNumber").value;
 
     if (validator.isValid(numerosCreditCard)) {
         alert("Valid credit card. Thank you for your purchase! Your ticket has been sent to your email :) ");
     } else {
-        alert("Your credit card isn't valid. Try again.");
+        alert("Your credit card " + validator.maskify(ccNumber.value) + " isn't valid. Try again.");
     }
 
 });
